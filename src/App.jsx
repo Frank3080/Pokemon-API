5706537e5e9f8da2aba8a96ac8415191c98c1bd3
Frank3080 <147672388+Frank3080@users.noreply.github.com>
2023-12-06 16:12:48 +0100

    second

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { useState, Suspense } from "react";
 import ErrorBoundary from "./ErrorBoundary";
 import PokemonCard from "./components/PokemonCard";
 import PokemonGrid from "./components/PokemonGrid";
+import "./index.css";
 
 function App() {
   const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [isDark, setIsDark] = useState(false);
+
   const url = "https://pokeapi.co/api/v2/pokemon/";
 
   function handleSelectPokemon(pokemon) {
@@ -13,21 +16,26 @@ function App() {
     };
   }
   return (
-    <ErrorBoundary fallback={<div>Error...</div>}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <div className="App">
-          {selectedPokemon ? (
-            <PokemonCard
-              parentUrl={url}
-              selectedPokemon={selectedPokemon}
-              clearHandler={() => setSelectedPokemon(null)}
-            />
-          ) : (
-            <PokemonGrid url={url} handleSelectPokemon={handleSelectPokemon} />
-          )}
-        </div>
-      </Suspense>
-    </ErrorBoundary>
+    <div className="app" data-theme={isDark ? "dark" : "light"}>
+      <ErrorBoundary fallback={<div>Error...</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <div className="App">
+            {selectedPokemon ? (
+              <PokemonCard
+                parentUrl={url}
+                selectedPokemon={selectedPokemon}
+                clearHandler={() => setSelectedPokemon(null)}
+              />
+            ) : (
+              <PokemonGrid
+                url={url}
+                handleSelectPokemon={handleSelectPokemon}
+              />
+            )}
+          </div>
+        </Suspense>
+      </ErrorBoundary>
+    </div>
   );
 }
 
